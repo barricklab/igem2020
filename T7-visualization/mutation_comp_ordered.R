a4f3b675bb7864c7f75ac16c543e96a6eb8b282d
amacaskill98 <52052927+amacaskill98@users.noreply.github.com>
2020-08-05 15:02:07 -0500

    input filename now set as variable "input.prefix" instead of hardcoded

--- T7-visualization/mutation_comp_ordered.R
+++ T7-visualization/mutation_comp_ordered.R
@@ -1,9 +1,34 @@
 require(ggplot2)
 require(tidyverse)
 require(plotly)
+
+##shell stuff##
+#!/usr/bin/env Rscript
+#add the name of the mutant file, without the .counts.tsv extension part
+#ex: input.prefix = "T7_replace_10_with_sfGFP"
+#after you define the line above, you should be able to run the script in one swift go, assuming you have the wt and disp files in your directory 
+option.input.prefix = input.prefix
+mt = data.frame()
+######################################
+# check that file exists 
+if (file.exists(paste0(option.input.prefix, ".counts.tsv"))) {
+  mt <- read_tsv(paste0(option.input.prefix, ".counts.tsv"), col_names=TRUE )
+} 
+
+
+# Default to the same output prefix as input prefix. 
+if (exists("output.prefix")) {
+  option.output.prefix = output.prefix
+} else {
+  option.output.prefix = input.prefix
+}
+
+################################
+
+
 #read in both the wild type and mutant phage count files, files names may vary 
-wt = read_tsv("T7-WT-3255.counts.tsv")
-mt = read_tsv("T7_replace_10_with_sfGFP.counts.tsv")
+wt = read_tsv("T7-WT_24.counts.tsv")
+
 disp = read.csv("gene_display.csv", stringsAsFactors = FALSE)
 
 #filter for all of the genes at the maximum time point of the simulation 
@@ -32,4 +57,8 @@ compare_genomes_barplot
 #generates plotly for the graph and saves it locally in the working directory 
 library(plotly)
 pPlotly <- ggplotly(compare_genomes_barplot)
+<<<<<<< HEAD
+htmlwidgets::saveWidget(pPlotly, "delete_10_compare_genomes.html")
+=======
 htmlwidgets::saveWidget(pPlotly, "compare_genomes.html")
+>>>>>>> 36dadf7640eb89bf0a00760f73153bd9d1feec90
