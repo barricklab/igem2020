285a3804ba31f21852eea18aa93edc7656e4ae8b
amacaskill98 <52052927+amacaskill98@users.noreply.github.com>
2020-08-13 13:46:29 -0500

    takes potential alternate model names as input now

--- T7-simulation/TACC/generate_pinetree_commands.py
+++ T7-simulation/TACC/generate_pinetree_commands.py
@@ -2,7 +2,11 @@ import random
 
 genome_filename = input("Enter name of .gb file you will be using: ")
 output_prefix = input("Enter prefix for output files: ")
-
+alt = input("Are you using an alternate model? (y/n): ")
+if alt == "y" or  alt == "Y":
+    model = input("Enter model name: ")
+else:
+    model = "phage_model.py"
 run_num = 0
 
 f = open("commands.sh", "w")
@@ -10,7 +14,7 @@ f = open("commands.sh", "w")
 for i in range(96):
     run_num += 1
     seed = random.randint(0,4294967295)
-    command = "python3 " + "$WORK/src/igem2020/T7-simulation/phage_model.py" + " -i " + genome_filename + " -o " + output_prefix + "_" + str(run_num) + " -s " + str(seed) + " -r\n"
+    command = "python3 " + "$WORK/src/igem2020/T7-simulation/" + model + " -i " + genome_filename + " -o " + output_prefix + "_" + str(run_num) + " -s " + str(seed) + " -r\n"
     f.write(command)
 
 f.close()
