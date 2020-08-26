f8dbcd4f893012b938862131a6d5cb81cccf8bda
amacaskill98 <52052927+amacaskill98@users.noreply.github.com>
2020-08-26 13:51:53 -0500

    added time parameter

--- T7-simulation/TACC/generate_pinetree_commands.py
+++ T7-simulation/TACC/generate_pinetree_commands.py
@@ -7,6 +7,7 @@ if alt == "y" or  alt == "Y":
     model = input("Enter model name: ")
 else:
     model = "phage_model.py"
+time = input("Enter time duration for simulaiton: ")
 run_num = 0
 
 f = open("commands.sh", "w")
@@ -14,7 +15,7 @@ f = open("commands.sh", "w")
 for i in range(96):
     run_num += 1
     seed = random.randint(0, 2147483647)
-    command = "python3 " + "$WORK/src/igem2020/T7-simulation/" + model + " -i " + genome_filename + " -o " + output_prefix + "_" + str(run_num) + " -s " + str(seed) + " -r\n"
+    command = "python3 " + "$WORK/src/igem2020/T7-simulation/" + model + " -i " + genome_filename + " -o " + output_prefix + "_" + str(run_num) + " -t " + str(time) + " -s " + str(seed) + " -r\n"
     f.write(command)
 
 f.close()
