9f8744ed9c686e5fe3f9e38359b46f91366e0f11
amacaskill98 <52052927+amacaskill98@users.noreply.github.com>
2020-09-18 19:22:53 -0500

    seperates gfp bar from main plot, adds titles and fixes label spacing

--- T7-visualization/avg_expression_graph.R
+++ T7-visualization/avg_expression_graph.R
@@ -1,8 +1,10 @@
 library(plyr)
 library(tidyverse)
 library(ggplot2)
-
-# Make sure to specify output.data as "protein" or "transcript" BEFORE you run this script! 
+library(cowplot)
+# 
+# Make sure to specify output.data as "protein" or "transcript" and set your working directory
+# to the folder containing your data BEFORE you run this script! 
 
 # get filenames
 filenames = list.files(pattern = "*.tsv")
@@ -36,24 +38,55 @@ if (output.data == "transcript"){
 
 disp = disp %>% arrange(sort.order)
 
-cts.to.graph$species = factor(cts.to.graph$species, levels = disp$species) 
+cts.to.graph$species = factor(cts.to.graph$species, levels = disp$species)
+
+cts.to.graph.GFP = cts.to.graph %>% filter(species == 'sfGFP')
+cts.to.graph.native = cts.to.graph %>% filter(species != 'sfGFP')
+
 
 #plots
 
-compare_genomes_barplot <- ggplot(cts.to.graph, aes(x = species, y = avg, fill = as.factor(gene.class))) + 
+native.plot <- ggplot(cts.to.graph.native, aes(x = species, y = avg, fill = as.factor(gene.class))) + 
   geom_bar(alpha = 0.5, stat = "identity") +
   geom_errorbar(aes(ymin = lower95, ymax = upper95), width = 0.5, alpha = 0.5) +
-  scale_fill_manual(values = c("#CC79A7", "#56B4E9", "#E69F00"), name = "Class", labels = c("I", "II", "III")) +
-  theme(axis.text.x = element_text(angle = 90), axis.line = element_line(colour = "black"),
+  scale_fill_manual(values = c("#CC79A7", "#56B4E9", "#E69F00"), name = "Class", labels = c("I", "II", "III", "GFP")) +
+  scale_y_continuous(expand = expansion(mult = c(0, .1))) +
+  theme(axis.text.x = element_text(angle = 90, vjust=0.5), axis.line = element_line(colour = "black"),
+        axis.title.y = element_blank(),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         panel.border = element_blank(),
         panel.background = element_blank())
-compare_genomes_barplot
 
+
+GFP.plot <- ggplot(cts.to.graph.GFP, aes(x = species, y = avg, fill = as.factor(gene.class))) + 
+  geom_bar(width = 0.2, alpha = 0.5, stat = "identity") +
+  geom_errorbar(aes(ymin = lower95, ymax = upper95), width = 0.125, alpha = 0.5) +
+  scale_fill_manual(values = "#3cf013") +
+  scale_y_continuous(expand = expansion(mult = c(0, .1))) +
+  theme(axis.text.x = element_text(angle = 90, vjust=0.5), axis.line = element_line(colour = "black"),
+        legend.position="none",
+        axis.title.x = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank())
+
+if (output.data == "transcript"){
+  native.plot = native.plot + ggtitle("Average transcript profile of T7 Bacteriophage genome at 1500 seconds")
+  GFP.plot = GFP.plot + ylab("Average Transcripts")
+}
+if (output.data == "protein"){
+  native.plot = native.plot + ggtitle("Average protein profile of T7 Bacteriophage genome at 1500 seconds")
+  GFP.plot = GFP.plot + ylab("Average Proteins")
+}
+
+
+plot.combined = plot_grid(GFP.plot, native.plot, rel_widths = c(1, 7), align = "h")
+plot.combined
 #generates plotly for the graph and saves it locally in the working directory
 #make sure to rename plotly file below
 library(plotly)
-pPlotly <- ggplotly(compare_genomes_barplot)
+pPlotly <- ggplotly(plot.combined)
 
-htmlwidgets::saveWidget(pPlotly, "MT-3.5-profile+RNAse-transcript.html")
+htmlwidgets::saveWidget(pPlotly, "test")
